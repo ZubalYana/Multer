0b6ce699644a2415d4918e7a00003866b1eb9de5
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-25 13:56:08 +0300

    connect to mongoDB and create the database

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose')
 const path = require('path')
 const multer = require('multer')
 
+mongoose.connect(`mongodb+srv://root:d19KQlNDbg1bWhR0@root.k7ankso.mongodb.net/?retryWrites=true&w=majority&appName=root`)
+.then(()=>{
+    console.log(`Connected to mongo DB`)
+})
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.get('/', (req, res)=>{
